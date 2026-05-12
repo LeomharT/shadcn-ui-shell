93f0e56e438cfd4e45941b5ecbeb0c58e60386ea
LeomharT <leo.liao.lzy@gmail.com>
2026-05-12 14:07:39 +0800

    feat(api-keys): add API keys table and management tabs

---
+++ src/pages/APIKeys/APIKeysTable/intex.tsx
@@ -0,0 +1,69 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+type APIKeys = {
+  id: number;
+  name: string;
+  status: string;
+  tracking_id: string;
+  secret_key: string;
+  created: string;
+  last_used: string;
+  created_by: string;
+  permissions: string;
+};
+
+type Columns<V, T> = {
+  title: string;
+  tip?: string;
+  dataIndex: string | string[];
+  key: string;
+  width?: number;
+  render?: (value: V, record: T, index: number) => React.ReactNode;
+};
+
+type APIKeysTableProps = {
+  data: APIKeys[];
+  columns: Columns<APIKeys[keyof APIKeys], APIKeys>[];
+  rowKey: keyof APIKeys;
+  loading?: boolean;
+};
+
+export default function APIKeysTable(props: APIKeysTableProps) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {props.columns.map((c) => (
+            <TableHead
+              key={c.key}
+              style={{ minWidth: c.width }}
+              className={'whitespace-nowrap uppercase text-[12px] font-semibold min-w-41.25'}
+            >
+              {c.title}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {props.data.map((item) => (
+          <TableRow key={item[props.rowKey]}>
+            {props.columns.map((c, i) => (
+              <TableCell key={c.key} className='text-[13px] w-full'>
+                {c.render
+                  ? c.render(item[c.dataIndex as keyof (typeof props.data)[number]], item, i)
+                  : item[c.dataIndex as keyof (typeof props.data)[number]]}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
--- src/pages/APIKeys/index.tsx
+++ src/pages/APIKeys/index.tsx
@@ -1,12 +1,34 @@
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { IconPlus } from '@tabler/icons-react';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { IconEdit, IconPlus, IconTrash } from '@tabler/icons-react';
+import { useState } from 'react';
+import APIKeysTable from './APIKeysTable/intex';
+
+const mocked = [
+  {
+    id: 1,
+    name: 'Secret key',
+    status: 'Active',
+    tracking_id: 'key_DlYgOSGundGD2Aq1',
+    secret_key: 'sk-...1hAA',
+    created: 'Apr 13, 2026',
+    last_used: 'Never',
+    created_by: 'Leo Leomhart',
+    permissions: 'All',
+  },
+];
 
 export default function APIKeys() {
+  const [active, setActive] = useState('project');
+
+  const [data] = useState(mocked);
+
   return (
     <div>
-      <div className='flex flex-col gap-3'>
-        <div className='flex flex-row justify-between items-start'>
+      <header className='flex flex-col gap-3 mb-6'>
+        <div className='flex flex-row flex-wrap gap-3 justify-between items-start'>
           <h1 className='scroll-m-20 text-[20px] font-semibold tracking-tight text-balance'>
             API Keys
           </h1>
@@ -15,7 +37,75 @@ export default function APIKeys() {
             Create new secret key
           </Button>
         </div>
+        <Tabs value={active} onValueChange={setActive}>
+          <TabsList variant='line' className='p-0 gap-5'>
+            <TabsTrigger className='p-0 [&:after]:h-px' value='project'>
+              Project API Keys
+            </TabsTrigger>
+            <TabsTrigger className='p-0 [&:after]:h-px' value='user'>
+              User API Keys
+              <Badge className='rounded-xs bg-orange-50 text-orange-700 dark:bg-orange-950 dark:text-orange-300'>
+                Legacy
+              </Badge>
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
         <Separator />
+      </header>
+      <div>
+        <div className='flex flex-col gap-3'>
+          <p>You have permission to view and manage all API keys in this project.</p>
+          <p>
+            Do not share your API key with others or expose it in the browser or other client-side
+            code. To protect your account's security, OpenAI may automatically disable any API key
+            that has leaked publicly.
+          </p>
+          <p>
+            View usage per API key on the <a className='underline cursor-pointer'>Usage page.</a>
+          </p>
+        </div>
+      </div>
+      <div className='my-4.5'>
+        <APIKeysTable
+          rowKey='id'
+          data={data}
+          columns={[
+            { title: 'Name', key: 'Name', dataIndex: 'name' },
+            { title: 'Status', key: 'Status', dataIndex: 'status' },
+            { title: 'Tracking ID', key: 'Tracking ID', dataIndex: 'tracking_id', width: 195 },
+            { title: 'Secret Key', key: 'Secret Key', dataIndex: 'secret_key' },
+            { title: 'Created', key: 'Created', dataIndex: 'created' },
+            { title: 'Last used', key: 'Last used', dataIndex: 'last_used', tip: '' },
+            { title: 'Created by', key: 'Created by', dataIndex: 'created_by' },
+            { title: 'Permissions', key: 'Permissions', dataIndex: 'permissions', width: 80 },
+            {
+              title: '',
+              key: 'action',
+              dataIndex: 'action',
+              width: 100,
+              render: (_, r) => (
+                <div className='inline-flex'>
+                  <Button
+                    variant='ghost'
+                    size='icon'
+                    className='[&_svg]:w-5! [&_svg]:h-5!'
+                    onClick={() => console.log(r)}
+                  >
+                    <IconEdit />
+                  </Button>
+                  <Button
+                    variant='destructive'
+                    size='icon'
+                    className='[&_svg]:w-5! [&_svg]:h-5! not-hover:bg-transparent dark:not-hover:bg-transparent'
+                    onClick={() => console.log(r)}
+                  >
+                    <IconTrash />
+                  </Button>
+                </div>
+              ),
+            },
+          ]}
+        />
       </div>
     </div>
   );
