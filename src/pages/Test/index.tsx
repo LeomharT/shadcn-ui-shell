3ad0fe6fc55298fd655bfd41af68da075d7d8106
LeomharT <leo.liao.lzy@gmail.com>
2026-09-02 09:50:01 +0800

    feat: test page

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,38 +1,22 @@
-import mqtt from 'mqtt';
 import { useEffect, useRef } from 'react';
 
 export default function Test() {
-  const clientRef = useRef<mqtt.MqttClient>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  async function renderImage(canvas: HTMLCanvasElement) {
+    const res = await fetch('/16f01.pgm');
+    const data = res.blob();
+  }
 
   useEffect(() => {
-    const client = mqtt.connect('mqtt://192.168.5.142:7998');
+    if (!canvasRef.current) return;
 
-    client.on('connect', () => {
-      client.subscribe('presence', (err) => {
-        if (!err) {
-          client.publish('presence', 'Hello mqtt');
-        }
-      });
-    });
-
-    client.on('message', (topic, message) => {
-      console.log(topic);
-      // message is Buffer
-      console.log(message.toString());
-      client.end();
-    });
-
-    client.on('error', (err) => {
-      console.warn(err);
-      client.end();
-    });
-
-    clientRef.current = client;
-
-    return () => {
-      clientRef.current?.end();
-    };
+    renderImage(canvasRef.current);
   }, []);
 
-  return <div>Test</div>;
+  return (
+    <div>
+      <canvas />
+    </div>
+  );
 }
